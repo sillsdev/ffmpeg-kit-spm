d97a208d3d22502f415215b88d101a00d83598d2
davidmoore1 <david_moore1@sil.org>
2025-10-31 15:02:07 -0400

    Creating release for v5.1.13

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let release = "v5.1.12"
+let release = "v5.1.13"
 
-let frameworks = ["ffmpegkit": "521c08b5b885e1be17a8eb676d8cf4cd3125763671c6bc49d3dfa61cbf0aa941", "libavcodec": "58c135b9587f92835ec9a7a6a49bffe1b8bc0cf480e60e6072cb507c34a36153", "libavdevice": "7b22e4d7d3790fa8c6b5f81987004c4cc97396aa4cc89d5b91f916f8e0c3985f", "libavfilter": "5af7631a53303ea8b3781b7d4a8b4255146bfd5556d2c2858283eebae7b1afa8", "libavformat": "584bd6e961c3d9f377c1b879485dcb672935138f115735d61b18521001ced223", "libavutil": "a653a4b81e06aa7986146bc3ba7c3b084a2d4040f2be9e982ace48dfd14515b6", "libswresample": "65c4e282f690c7d5faf2ba2dfa2864e3390c5d665b2d2ed4a7cea1ce382313e0", "libswscale": "998bbb50e8a4a43ebb55e83373f7f16e612e138b58fea2504c21519dc5e0cb94"]
+let frameworks = ["ffmpegkit": "cfcd0c00fbbd6222fa37afbad30000988cfd339bd2aa67cc091dbad7f51b8014", "libavcodec": "223231571e79de02703734cef01cf85985f796bb4abd97f55bb2e30db2726dfd", "libavdevice": "2d2b67d0b19deeea4029906314c4e4959a5aba2f2bb9779025d5e85d7838bb8a", "libavfilter": "4e992aded999a59d7c75869f4fb01d4d595c48bf580de9c7e6d8fc7dcae68ecb", "libavformat": "e8dae2ba40e94833e7ec4cc62d491c858bde18f485eae5b93837e31b45aabda1", "libavutil": "b767ca548ff5a58f410e0b05f832f9634c8d9a2d80004d0911ecf716fcc27fc2", "libswresample": "4e7ff5fba8e14e6862706268b87d1aa586ae6658a0d581e3367a5d765c35fe11", "libswscale": "94d373a2157937a0b29b56b7d9d08dfea3b3ac1a5896c2048a59e8bda41c0365"]
 
 func xcframework(_ package: Dictionary<String, String>.Element) -> Target {
     let url = "https://github.com/sillsdev/ffmpeg-kit-spm/releases/download/\(release)/\(package.key).xcframework.zip"
@@ -28,7 +28,7 @@ let libAVFrameworks = frameworks.filter({ $0.key != "ffmpegkit" })
 
 let package = Package(
     name: "ffmpeg-kit-spm",
-    platforms: [.iOS(.v15_6), .macOS(.v10_15), .tvOS(.v11), .watchOS(.v7)],
+    platforms: [.iOS(.v15), .macOS(.v10_15), .tvOS(.v11), .watchOS(.v7)],
     products: [
         .library(
             name: "FFmpeg-Kit",
